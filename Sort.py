cd647c48df0a62da632b1e49e8f2d52f600d34de
devynchew <devynchew@gmail.com>
2022-01-18 08:15:41 +0800

    Sort by ascending value done

--- Sort.py
+++ Sort.py
@@ -7,7 +7,7 @@ class Sort():
             expressions = []
             for line in file:
                 line.rstrip() # remove trailing whitespaces
-                expression = (line.split(' ')[0], line.split(' ')[1][:-1])
+                expression = (line.split(' ')[0], float(line.split(' ')[1][:-1]))
                 print(expression)
                 expressions.append(expression)
             print(expressions)
@@ -19,7 +19,7 @@ class Sort():
 
         while not isSorted:
             isSorted = True # assume everything is sorted until a swap happens
-
+            print(len(l))
             for i in range(len(l)-1): # end at 2nd last element to compare with last element
                 if l[i][1]>l[i+1][1]:
                     l[i],l[i+1], = l[i+1], l[i]
